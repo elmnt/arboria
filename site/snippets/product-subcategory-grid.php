064337ef86e11be3faab00e307d7cf8f48324ddf
Greg <greg@elmnt.com>
2017-03-08 15:58:54 -0800

    Fix image URLs in product-subcategory-grid.php - 12

--- site/snippets/product-subcategory-grid.php
+++ site/snippets/product-subcategory-grid.php
@@ -40,19 +40,18 @@ foreach($page->images()->filterBy('filename', '*=', 'subcat_') as $img){
 <ul class="product-grid__list cf" id="prod_sort_list">
   <?php foreach($page->children()->visible() as $product): ?><li class="product-grid__item" data-title="<?php echo $product->title()->html() ?>" data-date="<?php echo $product->date('Y-m-d') ?>" data-price="<?php echo $product->price()->html() ?>">
 
-    <?php /*if($image = $product->images()->sortBy('sort', 'asc')->first()):*/ ?>
-    <?php if($image = $page->images()->filterBy('filename', '*=', 'subcat_')): ?>
+    <?php foreach($product->images()->filterBy('filename', '*=', 'subcat_') as $img): ?>
     <a href="<?php echo $product->url() ?>">
       <picture class="fit">
         <!-- <div class="product-grid__image--holder"> -->
-        <source srcset="<?php echo $image->url() ?>" media="(min-width: 600px)">
-        <source srcset="<?php echo $image->resize(600)->url() ?>" media="(min-width: 400px)">
-        <source srcset="<?php echo $image->resize(300)->url() ?>" media="(min-width: 100px)">
-        <img class="product-grid__image" srcset="<?php echo $image->resize(600)->url() ?>" alt="<?php echo $product->title()->html() ?>">
+        <source srcset="<?php echo $img->url() ?>" media="(min-width: 600px)">
+        <source srcset="<?php echo $img->resize(600)->url() ?>" media="(min-width: 400px)">
+        <source srcset="<?php echo $img->resize(300)->url() ?>" media="(min-width: 100px)">
+        <img class="product-grid__image" srcset="<?php echo $img->resize(600)->url() ?>" alt="<?php echo $product->title()->html() ?>">
         <!-- </div> -->
       </picture>
     </a>
-    <?php endif ?>
+    <?php endforeach ?>
 
     <div class="product-grid__wrap">
     <h5 class="product-grid__title"><a href="<?php echo $product->url() ?>"><?php echo $product->title()->html() ?></a></h5>
