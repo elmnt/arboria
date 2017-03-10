0078a6c6ed5a0b037e292680f77cf5525b7b597b
Greg <greg@elmnt.com>
2017-03-09 16:12:07 -0800

    Testing multiple recipients on contact form

--- site/controllers/default.php
+++ site/controllers/default.php
@@ -9,7 +9,13 @@ return function($site, $pages, $page) {
         'actions' => [
             [
                 '_action' => 'email',
-                'to'      => 'sales@arboria.com',
+                'to'      => 'elmnt.gws@gmail.com',
+                'sender'  => 'info@dev.arboria.com',
+                'subject' => 'New message from the Arboria Contact Us form'
+            ],
+            [
+                '_action' => 'email',
+                'to'      => 'elmnt.public@gmail.com',
                 'sender'  => 'info@dev.arboria.com',
                 'subject' => 'New message from the Arboria Contact Us form'
             ]
