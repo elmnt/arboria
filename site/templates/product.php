ec7aef341a417567cb70cabc45e48fe6f75ce651
Greg <greg@elmnt.com>
2017-03-14 10:18:47 -0700

    Update /templates/product.php to exclude the subcat_ image - 2

--- site/templates/product.php
+++ site/templates/product.php
@@ -26,10 +26,10 @@
         <?php /* Get the first image */ ?>
         <a class="thumbnail gallery" href="<?php echo $page->images()->filterBy('extension', 'jpg')->not('filename', '*=', 'subcat_')->first()->url() ?>">
         <picture class="fit">
-          <source srcset="<?php echo $page->images()->filterBy('extension', 'jpg')->first()->url() ?>" media="(min-width: 800px)">
-          <source srcset="<?php echo $page->images()->filterBy('extension', 'jpg')->first()->resize(600)->url() ?>" media="(min-width: 400px)">
-          <source srcset="<?php echo $page->images()->filterBy('extension', 'jpg')->first()->resize(400)->url() ?>" media="(min-width: 100px)">
-          <img srcset="<?php echo $page->images()->filterBy('extension', 'jpg')->first()->resize(400)->url() ?>" alt="<?php echo $page->title()->html() ?>">
+          <source srcset="<?php echo $page->images()->filterBy('extension', 'jpg')->not('filename', '*=', 'subcat_')->first()->url() ?>" media="(min-width: 800px)">
+          <source srcset="<?php echo $page->images()->filterBy('extension', 'jpg')->not('filename', '*=', 'subcat_')->first()->resize(600)->url() ?>" media="(min-width: 400px)">
+          <source srcset="<?php echo $page->images()->filterBy('extension', 'jpg')->not('filename', '*=', 'subcat_')->first()->resize(400)->url() ?>" media="(min-width: 100px)">
+          <img srcset="<?php echo $page->images()->filterBy('extension', 'jpg')->not('filename', '*=', 'subcat_')->first()->resize(400)->url() ?>" alt="<?php echo $page->title()->html() ?>">
         </picture>
         </a>
         <figcaption class="mb0"><p><i class="fa fa-search-plus" aria-hidden="true"></i>Click the image to enlarge</p></figcaption>
