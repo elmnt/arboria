72ecd753f0287c65ac0e5b37960c0a2f12372745
Greg <greg@elmnt.com>
2017-03-16 15:51:07 -0700

    Turned on Google Analytics

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -29,19 +29,15 @@ error_reporting(E_ALL);
 
 <link rel="alternate" type="application/rss+xml" href="<?php echo url('feed') ?>" title="Blog Feed" />
 
-<!-- Google Analytics -->
 <script>
-/*
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-XXXXX-Y', 'auto');
+ga('create', 'UA-689321-2', 'auto');
 ga('send', 'pageview');
-*/
 </script>
-<!-- /Google Analytics -->
 
 </head>
 
